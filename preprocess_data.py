e6cdd835157491f73ac2c19f293b4e1ed8905243
Jia Ji <jiaji010616@outlook.com>
2025-03-22 02:27:23 +0000

    Create preprocess_data.py, the output will be save in splitted_data.

---
+++ preprocess_data.py
@@ -0,0 +1,107 @@
+import scipy.io
+import numpy as np
+import pandas as pd
+from skmultilearn.model_selection import iterative_train_test_split
+import numpy as np
+
+# Load .mat file
+mat_file_path = "./PPG_data/Compiled/PPGECG_all.mat"
+mat_data = scipy.io.loadmat(mat_file_path)
+
+PPG_ECG = mat_data['S']
+labels = mat_data['labels']
+PPG = PPG_ECG[:, :, 0]
+ECG = PPG_ECG[:, :, 1]
+PPG = np.transpose(PPG)
+ECG = np.transpose(ECG)
+
+PPG = np.expand_dims(PPG, axis=1)
+PPG = np.expand_dims(PPG, axis=1)
+
+# load .csv file
+csv_file_path = "./PPG_data/Compiled/PPGECG_all_table.csv"
+df = pd.read_csv(csv_file_path)
+df['index'] = df.index
+
+summary = (
+    df.groupby(['ID0', 'ECGcat'])
+    .size()
+    .reset_index(name='count')
+)
+
+pivot = summary.pivot(index='ID0', columns='ECGcat', values='count').fillna(0)
+X = np.array(pivot.index).reshape(-1, 1)  
+y = pivot.values  
+
+# First split: train (60%) vs temp (40%)
+X_train, y_train, X_temp, y_temp = iterative_train_test_split(X, y, test_size=0.4)
+
+# Second split: val (20%) vs test (20%) from temp
+X_val, y_val, X_test, y_test = iterative_train_test_split(X_temp, y_temp, test_size=0.5)
+
+train_patients = X_train.flatten()
+val_patients = X_val.flatten()
+test_patients = X_test.flatten()
+
+df_train = df[df['ID0'].isin(train_patients)]
+df_val = df[df['ID0'].isin(val_patients)]
+df_test = df[df['ID0'].isin(test_patients)]
+
+df_train_index = df_train["index"].tolist()
+df_val_index = df_val["index"].tolist()
+df_test_index = df_test["index"].tolist()
+
+x_train = PPG[df_train_index]
+x_val = PPG[df_val_index]
+x_test = PPG[df_test_index]
+
+y_train = df[df['ID0'].isin(train_patients)]["ECGcat"]
+y_val = df[df['ID0'].isin(val_patients)]["ECGcat"]
+y_test = df[df['ID0'].isin(test_patients)]["ECGcat"]
+
+print(f"The shape of x_train is {x_train.shape}")
+print(f"The shape of x_val is {x_val.shape}")
+print(f"The shape of x_test is {x_test.shape}")
+print(f"The shape of y_train is {y_train.shape}")
+print(f"The shape of y_val is {y_val.shape}")
+print(f"The shape of y_test is {y_test.shape}")
+
+
+
+#  check stratification
+
+print( 'Check stratification.......')
+def show_distribution(df_split, name):
+    dist = df_split['ECGcat'].value_counts(normalize=True) * 100
+    print(f"\n{name} ECG distribution (%):")
+    print(dist.round(2))
+
+show_distribution(df_train, "Train")
+show_distribution(df_val, "Validation")
+show_distribution(df_test, "Test")
+
+# Should be empty sets
+print(set(train_patients) & set(val_patients))
+print(set(train_patients) & set(test_patients))
+print(set(val_patients) & set(test_patients))
+
+command = input("Enter your command (yes/no): ")
+
+
+if command == "yes":
+    with open('./PPG_data/splitted_data/x_train.npy', 'wb') as f:
+        np.save(f, x_train)
+    with open('./PPG_data/splitted_data/y_train.npy', 'wb') as f:
+        np.save(f, y_train)
+    with open('./PPG_data/splitted_data/x_val.npy', 'wb') as f:
+        np.save(f, x_val)
+    with open('./PPG_data/splitted_data/y_val.npy', 'wb') as f:
+        np.save(f, y_val)
+    with open('./PPG_data/splitted_data/x_test.npy', 'wb') as f:
+        np.save(f, x_test)
+    with open('./PPG_data/splitted_data/y_test.npy', 'wb') as f:
+        np.save(f, y_test)
+elif command == "no":
+    pass
+else:
+    print("Please enter valid command.")
